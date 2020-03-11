38273121c86303df6debbf5057a6086ffb2dc882
Lucas Elias da Costa <lecosta@magna>
2020-03-11 11:15:23 -0300

    Version 0.0.3

--- api.py
+++ api.py
@@ -5,9 +5,9 @@ APY_KEY = "AIzaSyD8eH8oVJgE4FeDTkSaU50z4XZL-q9mXaI"
 google_api = googlemaps.Client(key = APY_KEY )
 api = GooglePlaces(APY_KEY)
 
-def getPlaces(adress):
+def getPlaces(adress,place_type):
 
-	
+	find_range = 100
 
 	adress_json = google_api.geocode(adress)
 
@@ -19,8 +19,14 @@ def getPlaces(adress):
 
 	lat_lon = lat+","+lon
 
-	places = api.search_places_by_coordinate(lat_lon, "500", "restaurant")
+	places = api.search_places_by_coordinate(lat_lon, str(find_range), place_type)
+	empty = []
 
+	while(len(places) < 10 ):
+		find_range+= 177
+		places = api.search_places_by_coordinate(lat_lon, str(find_range), place_type)
+
+	print(len(places))
 	return places
 def getDetails(place,fields):
 	details = api.get_place_details(place['place_id'], fields)
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import googlemaps
 APY_KEY = "AIzaSyD8eH8oVJgE4FeDTkSaU50z4XZL-q9mXaI"
 google_api = googlemaps.Client(key = APY_KEY )
 api = GooglePlaces(APY_KEY)
+valid_types = [line.rstrip('\n') for line in open('valid_types.txt')]
+name_types =[line.rstrip('\n').replace("_"," ").title() for line in open('valid_types.txt')]
 
 app = Flask(__name__)
 
@@ -18,19 +20,19 @@ app = Flask(__name__)
 def main():
 
 
-    return render_template('index.html')
+    return render_template('index.html',valid_types=valid_types,name_types=name_types)
 
 @app.route('/', methods=['GET','POST'])
 def results():
     adress = request.form['adress']
+    type_places = request.form['time']
     #Executa a função de buscar endereços próximos.
-    places = getPlaces(adress)
-    place = places[0]
+    places = getPlaces(adress,type_places)
+    #place = places[0]
     fields = ['name', 'formatted_address', 'international_phone_number', 'website', 'rating', 'review']
-    details = getDetails(place,fields)
-    joaquim = "Bar do joaquim"
+    #details = getDetails(place,fields)
 
-    return render_template('results.html',places=places,fields=fields,details=details,joaquim=joaquim,api=api)
+    return render_template('results.html',places=places,fields=fields,api=api)
 
 if __name__ == "__main__":
     app.run()
